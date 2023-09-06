1286e08b539825139bf989e5c97f57e4dc386692
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-09-06 21:54:49 +0300

    using jquery

--- script.js
+++ script.js
@@ -30,15 +30,47 @@ function handleFullscreenChange() {
 // Add event listener for fullscreen change
 document.addEventListener('fullscreenchange', handleFullscreenChange);
 
+const imageElement = document.getElementById('image');
+const showImageButton = document.getElementById('showImage');
 
 function changeBackground() {
-  const imageUrl = 'https://source.unsplash.com/random';
-  document.body.style.backgroundImage = `url('${imageUrl}')`;
+
+  fetch('https://source.unsplash.com/random')
+      .then(response => {
+          if (response.ok) {
+            imageElement.src = response.url;
+            // imageElement.style.transition= 'opacity 3s'; 
+          
+            
+           
+            // imageElement.classList.toggle('animate');
+            // imageElement.classList.toggle('animate2');
+          } else {
+              console.error('Failed to fetch image from Unsplash.');
+          }
+      })
+      .catch(error => {
+          console.error('Error fetching image:', error);
+      });
 }
 
+let leftPosition = 0;
+
+function moveLeft() {
+  leftPosition -= 50; // Move 50 pixels to the left
+
+  // Animate the element's left position
+  $('#moving-element').animate({ left: leftPosition }, 1000); // Adjust the duration as needed
+}
+
+
+console.log("first")
 // Call the function initially
-changeBackground();
+// changeBackground();
+
+// setInterval(function () {element.innerHTML += "Hello"}, 1000);
+
 
 // Set an interval to change the background image every 2 seconds
-setInterval(changeBackground, 2000); // 2000 milliseconds = 2 seconds
+setInterval((changeBackground,moveLeft), 3000); 
 
