7d4b7384fc91173e2f908b440dfb86d8ca58b697
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-09-06 16:41:23 +0300

    removing button on  full screen change

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
- // JavaScript to handle the fullscreen functionality
- const fullScreenButton = document.getElementById('fullscreen-button');
+// JavaScript to handle the fullscreen functionality
+const fullscreenButton = document.getElementById('fullscreen-button');
 
- // Function to enter fullscreen mode
- function enterFullScreen() {
-     if (document.documentElement.requestFullscreen) {
-         document.documentElement.requestFullscreen();
-     } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-         document.documentElement.mozRequestFullScreen();
-     } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-         document.documentElement.webkitRequestFullscreen();
-     } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-         document.documentElement.msRequestFullscreen();
-     }
- }
+// Function to enter fullscreen mode
+function enterFullscreen() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    // Firefox
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    // Chrome, Safari and Opera
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    // IE/Edge
+    document.documentElement.msRequestFullscreen();
+  }
+}
 
- // Attach the enterFullscreen function to the button click event
- fullScreenButton.addEventListener('click', enterFullScreen);
+// Attach the enterFullscreen function to the button click event
+fullscreenButton.addEventListener('click', enterFullscreen);
 
- fullScreenButton.addEventListener('click', () => {
-  enterFullScreen();
-  fullScreenButton.classList.add('removed');
-});
+// Function to handle fullscreen change event
+function handleFullscreenChange() {
+  document.fullscreenElement !== null
+    ? fullscreenButton.classList.add('removed')
+    : fullscreenButton.classList.remove('removed');
+}
+
+// Add event listener for fullscreen change
+document.addEventListener('fullscreenchange', handleFullscreenChange);
